7520594357cccc4b3dee5c957b5456448bf01b89
Carson Kegley <carson.kegley@gmail.com>
2021-03-31 10:49:59 -0400

    Reset the problem to the starting state

--- src/Helper.test.ts
+++ src/Helper.test.ts
@@ -2,10 +2,7 @@ import { encypher } from "./Helper";
 
 describe("encypher pass phrase", ()=>{
     it("encypher the phrase", ()=>{
-        expect(encypher("raindeer")).toBe("nainden");
-        expect(encypher("snake")).toBe("anake");
-        expect(encypher("better")).toBe("betmn");
-        expect(encypher("kitty")).toBe("kitmy");
+       
     })
 
 })
--- src/Helper.ts
+++ src/Helper.ts
@@ -2,35 +2,10 @@ export function encypher(codePhrase: string) {
   const unEncryptedCharacters = Array.from(codePhrase);
   let encryptedCharacters = new Array(20);
   //declare variables here
-  let eArray:string[] = [];
-  let tArray:string[] = [];
 
-  let firstE:boolean = false;
   unEncryptedCharacters.forEach((character, index) => {
-    
-    if(character === "r"){
-      character = "n";
-    }
+  // do things here
 
-    if(character === "s"){
-      character = "a";
-    }
-
-    if(character === "e" ){
-      if(eArray[0] === "e"){
-        character = "";
-      }
-      eArray.push("e");
-    }
-
-    if(character === "t"){
-      if(tArray[0] === "t"){
-        character = "m";
-      }
-      tArray.push("t")
-    }
-
-    
     encryptedCharacters[index] = character;    
   });
 
