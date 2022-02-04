2e98eb68c7538a3399c238cff0b770ab5a10e795
Carson Kegley <carson.kegley@gmail.com>
2022-02-03 21:58:02 -0500

    Added Ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-import { encypher } from './Helper';
+import { Button, TextField } from "@mui/material";
+import React, { useState } from "react";
+import "./App.css";
+import { encypher } from "./Helper";
+
+
+
+
 
 function App() {
+
+  
+  const [inputValue, setInputValue] = useState<string>("");
+  const [displayValue, setDisplayValue] = useState<string>("");
+  function handleClick(value:string) {
+    setDisplayValue(encypher(value));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p style={{fontSize: "80px"}}>
-          The pass phrase is: {encypher("test cypher")}
-        </p>
-      </header>
+      <div className="App-header"></div>
+      <div className="App-body">
+        <h1>Cypher Input</h1>
+        
+        <TextField id="standard-basic" label="Standard" variant="standard" value={inputValue} onChange={(event) => setInputValue(event.target.value)}/>
+        <Button
+          variant="contained"
+          sx={{ backgroundColor: "#0a2342",  margin: "30px" }}
+          onClick={() => {handleClick(inputValue)}}
+          type="submit"
+        >
+          Generate
+        </Button>
+        
+        <h1>{displayValue}</h1>
+      </div>
     </div>
   );
 }
